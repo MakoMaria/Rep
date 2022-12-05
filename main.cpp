e392054cd681435224c16e191fb8cf76a5218a05
MakoMaria <91057694+MakoMaria@users.noreply.github.com>
2022-12-05 23:35:12 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+struct Car
+{
+    enum mark {BMW, Lada, Toyota, UAZ, GAZ};
+    enum color {White, Black, Red, Blue};
+    int count;
+    int price;
+    int able;
+    int sale;
+};
+int main()
+{
+    int arr[10];
+
+    // Заполняем массив с клавиатуры
+    for (int i = 0; i < 10; i++) {
+        std::cout << "[" << i + 1 << "]" << ": ";
     return 0;
 }
